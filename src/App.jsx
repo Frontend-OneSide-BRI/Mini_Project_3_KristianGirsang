ed36de31b8b046a4611c3ecf62b66e1fb7b9a32d
kristian-girsang <kristian.girsang@ui.ac.id>
2023-06-08 17:16:48 +0700

    Update App component with API configuration

--- src/App.jsx
+++ src/App.jsx
@@ -16,23 +16,31 @@ const App = () => {
   const { url } = useSelector((state) => state.home);
 
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, []);
 
-  const apiTesting = async () => {
-    fetchDataFromApi("/movie/popular").then((res) => {
+  const fetchApiConfig = async () => {
+    fetchDataFromApi("/configuration").then((res) => {
       console.log(res);
-      dispatch(getApiConfiguration(res));
+
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+      
+      dispatch(getApiConfiguration(url));
     });
   };
   return (
     <BrowserRouter>
-      <Header />
+      {/* <Header /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<PageNotFound />} />
+        <Route path="/search/:query" element={<SearchResult />} />
       </Routes>
-      <Footer />
+      {/* <Footer/> */}
     </BrowserRouter>
   );
 };
