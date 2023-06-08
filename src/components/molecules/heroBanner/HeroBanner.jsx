0d75df3b215500b5498b65269dc2f6119be0d32e
kristian-girsang <kristian.girsang@ui.ac.id>
2023-06-08 19:05:13 +0700

    Add styling to HeroBanner

--- src/components/molecules/heroBanner/HeroBanner.jsx
+++ src/components/molecules/heroBanner/HeroBanner.jsx
@@ -8,7 +8,7 @@ import ContentWrapper from "../contentWrapper/ContentWrapper";
 
 const HeroBanner = () => {
   const [background, setBackground] = useState("");
-  const [query, setQUery] = useState("");
+  const [query, setQuery] = useState("");
   const navigate = useNavigate();
   const { url } = useSelector((state) => state.home);
   const { data, loading } = useFetch("/movie/upcoming");
@@ -27,25 +27,35 @@ const HeroBanner = () => {
   };
 
   return (
-    <div>
+    <div className="w-full h-[450px] md:h-[700px] flex bg-[#04152d] items-center top-0 absolute">
       {!loading && (
-        <div>
-          <Img src={background} />
+        <div className="w-full h-full top-0 left-0 opacity-50 absolute overflow-hidden">
+          <Img
+            className={`w-full h-full object-cover object-center block`}
+            src={background}
+          />
+          <div className="w-full h-[300px] bg-gradient-to-t from-[#04152d] to-transparent absolute bottom-0 left-0"></div>
         </div>
       )}
-      <div></div>
       <ContentWrapper>
-        <div>
-          <span>Hello, friend.</span>
-          <span>Find a millions of movies and Shows here. </span>
-          <div>
+        <div className="flex flex-col items-center text-white text-center relative max-w-800 mx-auto">
+          <span className="title text-4xl font-bold mb-2 md:mb-0 md:text-7xl">
+            Hello, friend.
+          </span>
+          <span className="subTitle text-base font-medium mb-4 md:text-lg md:mb-4">
+            Find millions of movies and shows here.
+          </span>
+          <div className="w-full flex items-center">
             <input
               type="text"
               placeholder="Search for a movie or show..."
-              onChange={(e) => setQUery(e.target.value)}
+              className="w-full h-12 bg-white text-black outline-none border-0 rounded-l-xl  md:h-14 px-3 text-base md:text-lg"
+              onChange={(e) => setQuery(e.target.value)}
               onKeyUp={searchHandler}
             />
-            <button>Search</button>
+            <button className="w-24 h-12 bg-gradient-to-r from-blue-500 to-green-600 text-white outline-none border-0 rounded-r-xl rounded-l-none text-base md:w-36 md:h-14 md:text-lg">
+              Search
+            </button>
           </div>
         </div>
       </ContentWrapper>
