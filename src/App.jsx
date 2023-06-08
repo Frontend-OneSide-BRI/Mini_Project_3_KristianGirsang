dcc9a745410dfbe5cd293049bfb172104b6bacfc
kristian-girsang <kristian.girsang@ui.ac.id>
2023-06-08 11:56:26 +0700

    Setup Redux Toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React from "react";
-import MoviePage from "./pages/movies/movies";
-import "tailwindcss/tailwind.css";
+import React, { useEffect } from "react";
+import { fetchDataFromApi } from "./helper/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
 
 const App = () => {
-  return (
-    <div>
-      <MoviePage />
-    </div>
-  );
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = async () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    });
+  };
+  return <div>Total pages: {url?.total_pages}</div>;
 };
 
 export default App;
