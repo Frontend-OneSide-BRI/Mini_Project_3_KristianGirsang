17053b21c3d040f938c8848d6d1bb2046e65bd66
kristian-girsang <kristian.girsang@ui.ac.id>
2023-06-08 14:39:30 +0700

    import pages and components into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import { fetchDataFromApi } from "./helper/api";
 import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice";
 
+import Header from "./components/molecules/header/header";
+import Footer from "./components/molecules/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import MoviePage from "./pages/movies/movies";
+import PageNotFound from "./pages/404/PageNotFound";
+
 const App = () => {
   const dispatch = useDispatch();
   const { url } = useSelector((state) => state.home);
