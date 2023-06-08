d90d4f668e0d2f6e3e87dc2d33f5f99aab181769
kristian-girsang <kristian.girsang@ui.ac.id>
2023-06-08 22:32:36 +0700

    Add trending page

--- src/components/molecules/contentWrapper/ContentWrapper.jsx
+++ src/components/molecules/contentWrapper/ContentWrapper.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ContentWrapper = ({ children }) => {
   return (
-    <div className="w-full max-w-[1200px] my-0 mx-auto py-0 px-20px">
+    <div className="w-full flex flex-row justify-between max-w-[1200px] my-0 mx-auto py-0 px-20px">
       {children}
     </div>
   );
--- src/pages/home/Trending.jsx
+++ src/pages/home/Trending.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+
 import ContentWrapper from "../../components/molecules/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../components/molecules/switchTabs/SwitchTabs";
+import useFetch from "../../hooks/useFetch";
 
 const Trending = () => {
+  const [endpoint, setEndpoint] = useState("day");
+
+  const { data, loading } = useFetch(`/trending/all/${endpoint}`);
+
+  const onTabChange = (tab) => {
+    setEndpoint(tab === "Day" ? "day" : "week");
+  };
+
   return (
     <div className="relative mb-[70px]">
-      <ContentWrapper className={`flex items-center justify-between mb-[20px]`}>
+      <ContentWrapper className="flex items-center justify-between mb-[20px]">
         <span className="text-2xl text-white font-normal">Trending</span>
+        <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
       </ContentWrapper>
     </div>
   );
